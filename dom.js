f005779daa4ab3dbc700bb7c70558c9242f68388
Akhil Verma <70507574+akhilvofficial9@users.noreply.github.com>
2020-11-02 12:37:30 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,11 @@
+function showinfo(){
+      document.getElementById("info").innerHTML=navigator.appVersion;
+    //document.getElementById("info").innerHTML=navigator.appName;
+    //document.getElementById("info").innerHTML=location.href;
+    //document.getElementById("info").innerHTML=location.port;
+    //document.getElementById("info").innerHTML=location.hostname;
+}
+
+function openpage(){
+    window.location.assign("https:www.google.com");
+}
